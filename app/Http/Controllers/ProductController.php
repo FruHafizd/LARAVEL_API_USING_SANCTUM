7ffc33d9ff326792e942ba126e0c969717af7fd3
FruHafizd <muhammadhafidz12333@gmail.com>
2024-11-23 16:59:15 +0800

    Adding Migration,Model,Controller - PRODUCT

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Product::orderBy('name', 'ASC')->get();
+        return response()->json(
+            [
+                'status' => true,
+                'message' => 'Data Found',
+                'data' => $data
+            ]
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
